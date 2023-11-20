b5bebd2e59c39e1d204816a52ae9ad2301e10ec7
PosledniAmir <posledniamir@tuta.io>
2023-11-20 20:28:18 +0100

    Update tiny-basic.lisp

--- tiny-basic/tiny-basic.lisp
+++ tiny-basic/tiny-basic.lisp
@@ -37,6 +37,67 @@
   (reduce (lambda (result fn)
             (cond
               ((listp fn) `,(thread-list result fn))
-              (t `(,fn ,result))))
+              (t `(funcall ,fn ,result))))
           rest
           :initial-value val))
+
+(defunclass string-value ((value ""))
+  (:documentation "Represents string."))
+
+(defunclass const-expression ((value nil))
+  (:documentation "Represents constant expression."))
+
+(defunclass print-command ((expression nil))
+  (:documentation "Represents print command."))
+
+(defunclass run-command ()
+  (:documentation "Represents run command."))
+
+(defunclass goto-command ((value 0))
+  (:documentation "Represents goto command."))
+
+(defunclass state ((program nil))
+  (:documentation "Represents state of the program."))
+
+(defgeneric print-value (value)
+  (:documentation "Pretty prints value."))
+
+(defmethod print-value ((value string-value))
+  (format t "~a" value))
+
+(defun get-program-line (state line)
+  (cond
+    ((null state) nil)
+    (t (let ((fst (first state))
+             (rst (rest state)))
+         (cond
+           ((= (car fst) line) (second fst))
+           (t (get-program-line rst line)))))))
+
+(defgeneric eval-expression (expression)
+  (:documentation "Evaluates expression."))
+
+(defmethod eval-expressionn ((expr const-expression))
+  (value expr))
+
+(defgeneric run-command (state line command)
+  (:documentation "Runs command on line with state."))
+
+(defmethod run-command (state line (command print-command))
+  (-> command #'expression #'print-value)
+  (run-next-line state line))
+
+(defmethod run-command (state line (command run-command))
+  (let ((fst-line (first (program state))))
+    (run-command state (first fst-line) (second fst-line))))
+
+(defmethod run-command (state line (command goto-command))
+  (let ((target (get-program-line state (value command))))
+    (run-command state (value command) target)))
+
+(defun run-next-line (state line)
+  (let ((result (find-if (lambda (item) (> (first item) line))
+                         (program state))))
+    (cond
+      ((null result) state)
+      (t (run-command state (first result) (second result))))))
